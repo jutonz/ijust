e3c8caaa1d8b4e946532c8fa9030a70cdc2def04
Justin Toniazzo <jutonz42@gmail.com>
2015-12-09 22:08:23 -0500

    Adds new occurrence template

--- app/controllers/occurrences_controller.rb
+++ app/controllers/occurrences_controller.rb
@@ -3,8 +3,25 @@ class OccurrencesController < ApplicationController
   def new
     @thing = Thing.find params[:thing_id]
     @occurrence = @thing.occurrences.build
+  end
 
+  def create
+    @thing = Thing.find params[:thing_id]
+    @occurrence = @thing.occurrences.build occurrence_params
     if @occurrence.save
+      redirect_to thing_path(@thing)
+    end
+  end
+
+  def destroy
+    @occurrence = Occurrence.find params[:id]
+    @thing = @occurrence.thing
+
+    if @thing.occurrences.count == 1
+      @thing.delete
+      redirect_to root_url, message: "Thing deleted."
+    else
+      @occurrence.delete
       redirect_to thing_path(@thing)
     end
   end
@@ -12,6 +29,6 @@ class OccurrencesController < ApplicationController
   private
 
   def occurrence_params
-    params.require(:occurrence).permit(:thing_id)
+    params.require(:occurrence).permit(:thing_id, :created_at)
   end
 end
